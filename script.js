64cad5b94601e059fb79adca369e73d112735998
Pedro Henrique de Almeida <119445086+PeedroHAlmeida@users.noreply.github.com>
2024-04-14 22:05:58 -0300

    Ajuste para celulares

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ let windowWidth = window.innerWidth;
 let height = windowHeight - 50;
 let width = windowWidth - 50;
 
-// Verifica se a largura da tela é menor que a altura, o que sugere um dispositivo móvel
 if (windowWidth < windowHeight) {
-    height = windowHeight * 0.5; // Reduz a altura máxima para 50% da altura da tela
-    width = windowWidth * 0.5; // Reduz a largura máxima para 50% da largura da tela
+    height = windowHeight * 0.8;
+    width = windowWidth * 0.8;
 }
 
 a.addEventListener('mouseover', function (){
