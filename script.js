feee2bc328bc2242a5b79bb4213a6f3798e07b46
Pedro Henrique de Almeida <119445086+PeedroHAlmeida@users.noreply.github.com>
2024-04-22 20:55:39 -0300

    Mensagem descritiva do commit

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let a = document.getElementById('no');
 let windowHeight = window.innerHeight;
 let windowWidth = window.innerWidth;
-let height = windowHeight - 50;
-let width = windowWidth - 50;
 
-if (windowWidth < windowHeight) {
-    height = windowHeight * 0.8;
-    width = windowWidth * 0.8;
+// Defina margens para evitar elementos fora da tela em dispositivos móveis
+let heightMargin = 40; // Margem para a altura
+let widthMargin = 40; // Margem para a largura
+
+// Ajuste para celulares - menos espaço para elementos móveis
+let height = windowHeight - heightMargin;
+let width = windowWidth - widthMargin;
+
+if (windowWidth < windowHeight) { // Se a tela é mais alta do que larga
+    height = windowHeight * 0.7; // Ajuste para celular
+    width = windowWidth * 0.7; // Ajuste para celular
 }
 
 a.addEventListener('mouseover', function (){
-    a.style.position = "absolute";
-    a.style.top = Math.random() * height + "px";
-    a.style.left = Math.random() * width + "px";
+    a.style.position = "absolute"; // Define a posição absoluta
+    a.style.top = Math.random() * height + "px"; // Posição aleatória dentro da margem de altura
+    a.style.left = Math.random() * width + "px"; // Posição aleatória dentro da margem de largura
 });
